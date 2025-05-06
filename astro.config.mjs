2f99a0ab4768866d28942ebea139f90c0c7c5a1c
Matt Kevan <m@kevan.tv>
2025-05-06 17:24:58 +0100

    Update blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()],
+  integrations: [preact(), react(), markdoc()],
   vite: {
     plugins: [tailwindcss()],
   },
