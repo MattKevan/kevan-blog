b928d7646bdcfe0853c3c0cd34f1fc7eed5e4a1e
Matt Kevan <m@kevan.tv>
2025-05-09 12:13:59 +0100

    Add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,16 @@ import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [preact(), react(), markdoc()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
