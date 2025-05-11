80bb1dce20d12759615430cdd5a3aecdbadd1707
Matt Kevan <m@kevan.tv>
2025-05-11 18:51:36 +0100

    Rebuild package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import markdoc from "@astrojs/markdoc";
 
-import cloudflare from "@astrojs/cloudflare";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
@@ -15,7 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  
 
   adapter: netlify(),
 });
