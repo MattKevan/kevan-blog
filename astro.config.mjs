741b1a7b6fabecf92babca9484de643eccfa5d22
Matt Kevan <m@kevan.tv>
2025-05-12 10:23:50 +0100

    Add glitch gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import markdoc from "@astrojs/markdoc";
-
+import embeds from 'astro-embed/integration';
 import netlify from "@astrojs/netlify";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.kevan.tv",
-  integrations: [markdoc()],
+  integrations: [embeds(), markdoc(), mdx()],
 
   vite: {
     plugins: [tailwindcss()],
